014465af82af1a2777f9c10e5c54eef71bc51d36
Quang Dat Ho <114854289+ridgud@users.noreply.github.com>
2023-10-05 09:05:12 +0700

    Add SpringBoot + Dependencies

---
+++ src/main/java/com/example/MangaWebsite/MangaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.MangaWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MangaWebsite/MangaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MangaWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
